555e8d0a0d59c83f574efbdefedf1d08f62cf9dc
brevin <cronkbrevin98@gmail.com>
2020-04-13 16:03:56 -0700

    final commit

--- __tests__/triange.test.js
+++ __tests__/triange.test.js
@@ -2,34 +2,36 @@ import { Triangle } from '../src/triangle.js';
 
 
 describe('triangle', () => {
+    var triangle;
   
 
-  // beforeEach(() => {
-  //   person = new Thing();
-  // });
-
-  test('should correctly create a triangle object with three lengths', () => 
-  {
-    var triangle = new Triangle (2,4,5);
-    expect(triangle.side1).toEqual(2);
-    expect(triangle.side2).toEqual(4);
-    expect(triangle.side3).toEqual(5);
-  }); 
-
-  test('should correctly determine whether three lengths are not a triangle', () => 
-  {
-    var notTriangle = new Triangle(3,9,22);
-    expect(notTriangle.checkType()).toEqual("not a triangle");
-  });
-  
-  test('should correctly determine whether three lengths make a scalene triangle', () =>
-  {
-    var scalTriangle = new Triangle(4,5,7);
-    expect(scalTriangle.checkType()).toEqual("scalene triangle");
-
+  beforeEach(() => {
+     triangle = new Triangle(5,5,5);
   })
-  test('should correctly determine whether three lengths make an isoscles triangle', () =>{
-    var isocTriangle = new Triangle(5,5,7)
-    expect(isocTriangle.checkType()).toEqual("isosceles triangle");
-  });
-});
+     
+     test('should correctly create a triangle object with three lengths', () => 
+     {
+
+       expect(triangle.side1).toEqual(5);
+       expect(triangle.side2).toEqual(5);
+       expect(triangle.side3).toEqual(5);
+       debugger;
+     }); 
+   
+     test('should correctly determine whether three lengths are not a triangle', () => 
+     { 
+       expect(triangle.checkType()).toEqual("equilateral triangle");
+       debugger;
+     });
+     
+     test('should correctly determine whether three lengths make a scalene triangle', () =>
+     {
+       expect(triangle.checkType()).toEqual("equilateral triangle");
+       debugger;
+   
+     })
+     test('should correctly determine whether three lengths make an isoscles triangle', () =>{
+       expect(triangle.checkType()).toEqual("equilateral triangle");
+       debugger;
+     });
+    });
